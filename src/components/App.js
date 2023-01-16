58a9466dea2e5912b7e066bfbc8f099f1b39bd3d
moyeen_g <moyeengodyari@gmail.com>
2023-01-16 18:14:35 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p data-ns-test="project-name">Project Name: Kanban Task Manager</p>
+      <p data-ns-test="project-description">Description: A task management web app based on kanban methodologies,
+        in which user can create multiple kanban boards with each board having its separate set of tasks and subtasks,
+        categorised into three columns named "ToDo", "Doing" and "Done.
+      </p>
+    </div>
   )
 }
 
